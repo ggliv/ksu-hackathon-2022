a803905de37e8878ec8e977365b7c7d76ad9e6ca
Gabe Livengood <47010459+ggliv@users.noreply.github.com>
2022-09-16 15:52:49 -0400

    more more cleanup

--- src/WaterMonitorandWatering/WaterMonitorandWatering.ino
+++ src/WaterMonitorandWatering/WaterMonitorandWatering.ino
@@ -1,25 +1,23 @@
+#define WaterSensor A0
+#include "DHT.h"  
+#define DHTPIN 2
+#define DHTTYPE DHT11
 
-  #define WaterSensor A0
-  #include "DHT.h"  
-  #define DHTPIN 2
-  #define DHTTYPE DHT11
 
+float sensorValue = 0;
 
-  float sensorValue = 0;
+// Setup the water pump
+int motorPin = 3;
 
-  // Setup the water pump
-  int motorPin = 3;
-
-  // Setup the buzzer
-  const int buzzer = 9; // pin 9
-  DHT dht(DHTPIN,DHTTYPE);
+// Setup the buzzer
+const int buzzer = 9; // pin 9
+DHT dht(DHTPIN,DHTTYPE);
 
 void setup(){
   pinMode(motorPin,OUTPUT);
   pinMode(buzzer,OUTPUT);
   Serial.begin(9600);
   dht.begin();
-
 }
 
 void loop() {
@@ -33,7 +31,4 @@ void loop() {
   5 -> If they are too high, then do nothing
   6 -> If there is error, notify user by buzzer and led
   */
-
-
-
 }
